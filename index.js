0289304100422e754c0b60f11279ba951b749626
Valeria <valeria.grotto01@gmail.com>
2022-05-14 15:02:12 +0200

    Added db connection

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const app = require('./app/app.js');
+const mongoose = require('mongoose');
 
-const port = 3000;
+/**
+ * https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
+ */
+const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+// app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+/**
+ * Configure mongoose
+ */
+// mongoose.Promise = global.Promise;
+// app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+app.locals.db = mongoose.connect("mongodb+srv://admin:admin1234@cluster0.deuin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+.then ( () => {
+    
+    console.log("Connected to Database");
+    
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+    
+});
