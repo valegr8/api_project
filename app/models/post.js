2e4e242ecf2697b93d84c2c5e7e93a15acd7ff1f
Valeria <valeria.grotto01@gmail.com>
2022-05-22 13:27:08 +0200

    removed post_id, now we use only the id generated by mongoDb for the posts

--- app/models/post.js
+++ app/models/post.js
@@ -6,5 +6,4 @@ module.exports = mongoose.model('Post', new Schema({
 	title: String,
 	description: String,
 	createdBy: String,	//email of the user
-	post_id: Number 	//id of the post, used by the api because shorter then the one assigned by mongoDB (_id)
 }));
--- utils/utils.js
+++ utils/utils.js
@@ -1,96 +1,78 @@
-// UTILITY FUNCTIONS
-// const express = require('express');
+/** *******************
+ *  UTILITY FUNCTIONS 
+ ******************** */
 
-const u = {};
+const utils = {}; 
+
+const debug = 1;
+
+/**
+ * Debug print
+ */
+utils.printd = (string) => {
+	if(debug)
+		console.log(string);
+};
+
+/**
+ * Verifies validity of an id
+ */
+utils.isIdValid = (id) => {	
+	let res = true;
+	if(id == undefined || id == null || id <= 0)
+	{
+		res = false;
+		utils.printd('Id not valid');
+	}
+	return res;
+}
+
+/** ***************
+ *  STATUS CODES  
+ **************** */
 
 /**
  * Sets a 405 http response status, method not allowed
  */
-u.notAllowed = (res) => {
+ utils.notAllowed = (res) => {
+	utils.printd('Method Not Allowed');
 	res.status(405).json({status: 405, message: 'Method Not Allowed'});
 };
 
 /**
  * Sets a 400 http response status, bad request
  */
-u.badRequest = (res) => {
+ utils.badRequest = (res) => {
+	utils.printd('Bad Request');
 	res.status(400).json({status: 400, message: 'Bad Request'});
 };
 
 /**
  * Sets a 404 http response status, not found
  */
-u.notFound = (res) => {
+utils.notFound = (res) => {
+	utils.printd('Not Found');
 	res.status(404).json({status: 404, message: 'Not Found'});
-};
-
-/**
- * Debug print
- */
-u.printd = (a) => {
-	console.log(a);
-};
-
-/**
- * Verifies validity of an id
- */
-u.isIdValid = (id) => {	
-	let r = true;
-	let p = parseInt(id);
-	if(!id || (id && (isNaN(p))) || p <= 0)
-	{
-		r = false;
-	}
-	return r;
-}
-
-
-/**
- * Useful to answer get requests
- */
-u.addProp = (o,prop,value) => {
-	//Le prime due righe di codice sono
-	//importanti dunque non cancellatele
-	//pensando che siano ridondanti.
-	//In poche parole, il loro ruolo è
-	//quello di cambiare il metodo con cui
-	//il parametro verrà convertito in JSON.
-	let t = JSON.stringify(o);
-	o = JSON.parse(t);
-	o[prop] = value;//semplicemente aggiunge la proprietà.
-	return o;//è importante.
-};
-
-//Non è l'ottimo ma fa' il suo
-//lavoro.
-//Ovviamente se spegni il server
-//riparte da questo valore.
-//per le prove mettete il valore che volete
-let last_post_id = 0; 
-//Provides an id for a new post.
-u.generatePostId = () => {
-	last_post_id++;
-	return last_post_id;
 };
 
 /**
  * Sets a status, 200 ok or 404 Not found
  */
-u.setResponseStatus = (param,res) => {
-	if(param)
+utils.setResponseStatus = (param,res) => {
+	if(param) {
+		utils.printd('Success');
 		res.status(200).json({message: param});
+	}
 	else
-		u.notFound(res);
+		utils.notFound(res);
 };
 
 /**
  * Sets a 201 status code resource created
  */
- u.created = (param,res) => {
-	// if(param)
-		res.status(201).json({status: 201, message: 'Created'});
-	// else
-	// 	u.notFound(res);
+utils.created = (res) => {
+	utils.printd('Created');
+	res.status(201).json({status: 201, message: 'Created'});
 };
 
-module.exports = u;
+module.exports = utils;
