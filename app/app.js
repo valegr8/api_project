8dc37c219be86fdbf1221cc9111848ec5ad0693e
Daniele <daniele.dallanese@studenti.unitn.it>
2022-05-24 11:16:42 +0200

    Creazione della seconda versione dei files users.js e posts.js

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname+'../../public'));
 const users = require('./users.js');
 const usersV2 = require('./users_v2.js');
 const posts = require('./posts.js');
+const postsV2 = require('./posts_v2.js');
 const authentication = require('./authentication.js');
 const tokenChecker = require('./tokenChecker.js');
 
@@ -36,7 +37,7 @@ app.use('/api/v1/posts/', posts);
 app.use('/api/v1/users', users);
 
 //version 2
-app.use('/api/v2/posts/', posts);
+app.use('/api/v2/posts/', postsV2);
 app.use('/api/v2/users', usersV2);
 
 /**
