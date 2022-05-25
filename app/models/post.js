e8f943a7fe96e14be537660f331f621813d8824c
stefano <stefanomodenese.sm@gmail.com>
2022-05-25 16:51:31 +0200

    fixed bug regarding conflict between old version of Post schema and newest one

--- app/models/post.js
+++ app/models/post.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // set up a mongoose model
-module.exports = mongoose.model('Post', new Schema({
+module.exports = mongoose.model('Post_old', new Schema({
 	title: String,
 	description: String,
 	createdBy: String,	//email of the user
