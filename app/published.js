92bf0811b26d35e6b445e1a3a0d7f878313cf653
stefano <stefanomodenese.sm@gmail.com>
2022-05-28 13:17:39 +0200

    fixed paths of published routes

--- app/published.js
+++ app/published.js
@@ -4,35 +4,22 @@ const Post = require('./models/post_v2');
 const User = require('./models/user');
 
 const utils = require('../utils/utils.js');
-const { printd } = require('../utils/utils.js');
+const { printd, isValid } = require('../utils/utils.js');
 const { isValidObjectId } = require('mongoose');
 
-
-/**
- * Get all post created by an user
- */
- router.get('/:createdBy', async (req, res) => {
-	printd('[GET/:email]EMAIL: ' + req.params.createdBy);		
-	Post.find({ createdBy: req.params.createdBy }).exec().then((post)=>{
-	    utils.setResponseStatus(post,res);
-	}).catch((e) => {
-		printd('Error: ' + e);
-		utils.notFound(res);
-	});
-});
 
 /**
  * Delete one post
  */
 
-router.delete('/:id/:createdBy', async (req, res) =>{
+router.delete('/:email/posts/:id', async (req, res) =>{
     let post = await Post.findById(req.params.id).exec();
     if (!post) {
         res.status(404).send();
         console.log('post not found');
         return;
     }
-    if (post.createdBy != req.params.createdBy) {
+    if (post.createdBy != req.params.email) {
         return res.status(401).json({
             message: 'You can only delete your own posts'
         });
@@ -46,17 +33,18 @@ router.delete('/:id/:createdBy', async (req, res) =>{
 /**
  * Modify published post
  */
-router.put('/:id/:createdBy', async (req, res) =>{
+router.put('/:email/posts/:id', async (req, res) =>{
     let post = await Post.findById(req.params.id).exec();
     if (!post) {
         res.status(404).send();
         console.log('post not found');
         return;
     }
-    if (!isValidObjectId(req.body.title) || !isValidObjectId(req.body.description)) {
+    if (!isValid(req.body.title) || !isValid(req.body.description)) {
         utils.badRequest(res, 'Bad request: no modification info given');
+        return;
     }
-    if (post.createdBy != req.params.createdBy) {
+    if (post.createdBy != req.params.email) {
         return res.status(401).json({
             message: 'You can only modify your own posts'
         });
